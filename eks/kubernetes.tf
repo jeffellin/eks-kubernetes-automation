00b4a195cd8bc6ca6f65f3d086c1da929e87ad73
Jeff <11721615+jeffellin@users.noreply.github.com>
2025-08-20 20:28:11 -0400

    add argo deployment changes

--- eks/kubernetes.tf
+++ eks/kubernetes.tf
@@ -8,14 +8,22 @@ resource "kubernetes_service_account" "wiz_service_account" {
     }
   }
 
-  depends_on = [aws_eks_cluster.wiz_cluster]
+  depends_on = [aws_eks_cluster.wiz_cluster, aws_eks_node_group.wiz_node_group]
 }
 
 # ArgoCD Application for web-eks-kubernetes-automation
-resource "kubernetes_manifest" "argo_application" {
-  manifest = yamldecode(file("${path.module}/../k8s/argo-application.yaml"))
+# Using null_resource with kubectl to avoid REST client dependency issues
+resource "null_resource" "argo_application" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.wiz_cluster.name}
+      kubectl apply -f ${path.module}/../k8s/argo-application.yaml
+    EOT
+  }
   
   depends_on = [
+    aws_eks_cluster.wiz_cluster,
+    aws_eks_node_group.wiz_node_group,
     helm_release.argocd
   ]
 }
--- eks/main.tf
+++ eks/main.tf
@@ -29,21 +29,21 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.wiz_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.wiz_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = try(aws_eks_cluster.wiz_cluster.endpoint, "")
+  cluster_ca_certificate = try(base64decode(aws_eks_cluster.wiz_cluster.certificate_authority[0].data), "")
+  token                  = try(data.aws_eks_cluster_auth.cluster.token, "")
 }
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.wiz_cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.wiz_cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = try(aws_eks_cluster.wiz_cluster.endpoint, "")
+    cluster_ca_certificate = try(base64decode(aws_eks_cluster.wiz_cluster.certificate_authority[0].data), "")
+    token                  = try(data.aws_eks_cluster_auth.cluster.token, "")
   }
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.wiz_cluster.name
+  name = try(aws_eks_cluster.wiz_cluster.name, "")
 }
 
 data "aws_availability_zones" "available" {
