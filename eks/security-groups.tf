65a28b9b97a612408d41be02c6bd2c4e87da421a
Jeff <11721615+jeffellin@users.noreply.github.com>
2025-08-28 20:42:58 -0400

    terraform updates

--- eks/security-groups.tf
+++ eks/security-groups.tf
@@ -116,6 +116,14 @@ resource "aws_security_group" "wiz_postgres_sg" {
   }
 
   ingress {
+    description = "PostgreSQL from anywhere"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description     = "SSH from bastion"
     from_port       = 22
     to_port         = 22
